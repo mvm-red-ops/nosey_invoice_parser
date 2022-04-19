d53e846294c7f19d267fa1267c4f1cfba87ca950
taylor <tayloryoung@mvmediasales.com>
2022-04-19 10:26:43 -0400

    final

--- findData.js
+++ findData.js
@@ -1,10 +1,92 @@
+function findData(blob, page){
+  const platforms = ['Roku', 'Plex', 'Samsung', 'Samsung-mx', 'Samsung-br', 'Vizio', 'Xumo', 'Connector.', '(VOD2)']
+
+  let count = 0
+  let irrelevant = true;
+  const clean = []
+
+    
+  if(!blob.Texts) return
+  let data = blob.Texts;
+  let text;
+
+    while(irrelevant){
+      text = data[count]
+      if(!text["R"]) continue
+      else text = text["R"][0]["T"]
+
+      console.log(`irrelevant text ${irrelevant}`)
+      
+      if(!text.includes('Quantity')) count++
+      else irrelevant = false
+    }
+
+  
+
+   for(let i = count +1; i <  data.length - 1; i++){
+
+     let text = data[i]["R"][0]["T"]
+     console.log(`text: ${text}`)
+
+    
+
+    if(text.includes('%20')){
+      text = text.split('%20').join(' ')
+    }
+    if(text.includes('%2C')){
+      text = text.split('%2C').join('')
+    }
+    if(text.includes('%3A')){
+      text = text.split('%3A').join(':')
+      
+    }
+    
+    if(text.includes(':') ){
+      let textNext = data[i+1]["R"][0]["T"]
+
+      if(textNext.includes('%20')){
+        textNext = textNext.split('%20').join(' ')
+      }
+      if(textNext.includes('%2C')){
+        textNext = textNext.split('%2C').join('')
+      }
+      if(textNext.includes('%3A')){
+        textNext = textNext.split('%3A').join(':')
+      }
+      let word_array = textNext.split(' ')
+      let last_word = word_array[word_array.length -1]
+
+
+      
+      if(textNext.includes('Nosey') || platforms.includes(last_word)){
+        clean.push(`${text} ${textNext}`)
+        i++
+      } 
+      else {
+          clean.push(text)
+      }
+
+    } else {
+      clean.push(text)
+    }
+
+    count++
+  }
+  for(let i = 0; i < clean.length; i++){
+    let curr = clean[i] 
+    console.log(`curr: ${curr}`)
+  }
+  return {clean: clean, pastPage: page}
+}
+
 function organize (array){
     
     let rows = []
+    let idx = array.indexOf('Quantity')
 
-    for(let i = 0; i < array.length; i+=5){
+    for(let i = idx + 1; i < array.length; i+=5){
       let curr = array[i];
-      console.log(curr)
+
       let record = []
       for(let h = 0; h < 5; h++){
         record[h] = array[i + h]
@@ -13,12 +95,12 @@ function organize (array){
   
 
     }
-    console.log(rows)
+    // console.log(rows)
     return rows
 
 }
 
 
 module.exports = {
-  organize
+  organize,findData
 }
--- transformJSON.js
+++ transformJSON.js
@@ -1,79 +1,26 @@
 let fs = require('fs')
 const createCSV = require('./createCSV')
-const findData = require('./findData.js')
+const helpers = require('./findData.js')
 
 
-const platforms = ['Roku', 'Plex', 'Samsung', 'Samsung-mx', 'Samsung-br', 'Vizio', 'Xumo']
 function transformJSON(){
   const csvData = []
   fs.readdir('./json', function(err, items) {
+    let results = { clean: []}
     // iterate through each json file
-    for (var i=0; i < 1; i++) {
+    for (var i=2; i < 3; i++) {
       const json = require(`./json/${items[i]}`)
-      var body = json["Pages"][1]["Texts"]
+      var pages = json["Pages"]
       
-      const clean = []
-      let count = 0
-      let irrelevant = true;
-
-      while(irrelevant){
-        let text = body[count]["R"][0]["T"]
-        if(text.includes('Quantity')) irrelevant = false
-        else count++
+      let clean = []
+      for(let i = 0; i < pages.length; i++){
+        results = helpers.findData(pages[i], i)
+        clean = results && results.clean ?  [...clean, ...results.clean] : [...clean]
       }
 
-      for(let i = count +1; i <  body.length - 1; i++){
-        let text = body[i]["R"][0]["T"]
-
-        if(text.includes('%20')){
-          text = text.split('%20').join(' ')
-        }
-        if(text.includes('%2C')){
-          text = text.split('%2C').join('')
-        }
-        if(text.includes('%3A')){
-          text = text.split('%3A').join(':')
-        }
-
-        
-        
-        if(text.includes(':') ){
-          let textNext = body[i+1]["R"][0]["T"]
-
-          if(textNext.includes('%20')){
-            textNext = textNext.split('%20').join(' ')
-          }
-          if(textNext.includes('%2C')){
-            textNext = textNext.split('%2C').join('')
-          }
-          if(textNext.includes('%3A')){
-            textNext = textNext.split('%3A').join(':')
-          }
-          let word_array = textNext.split(' ')
-          let last_word = word_array[word_array.length -1]
 
 
-
-          if(textNext.includes('Nosey') || platforms.includes(last_word)){
-            clean.push(`${text} ${textNext}`)
-            i++
-          } 
-          else {
-              clean.push(text)
-          }
-
-        } else {
-          clean.push(text)
-        }
-
-        count++
-      }
-      for(let i = 0; i < clean.length; i++){
-        let curr = clean[i]
-        
-      }
-
-      const csvData = findData.organize(clean)
+      const csvData = helpers.organize(clean)
 
       let filename = items[i].split(' ')
       let month = filename[filename.length -1].split('.')[0]
